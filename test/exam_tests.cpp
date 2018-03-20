857510644ce6a2d016a7197cc10b41697c7c315b
alex <1070556n>
2018-03-19 17:01:55 -0700

    Initial Framework for Tuesday Exam

---
+++ test/exam_tests.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h
+
+class LabExamFixture : public ::testing::Test {
+
+protected:
+    virtual void TearDown() {
+
+    }
+
+    virtual void SetUp() {
+
+    }
+
+
+public:
+
+};
+
+
+TEST(crashTest, constructorTest) {
+
+}
+
+TEST_F(LabExamFixture, basicNextToken) {
+
+}
+
+TEST_F(LabExamFixture, multiDigitNextToken) {
+
+}
+
+TEST_F(LabExamFixture, multiDigitCurrentToken) {
+
+}
+
+TEST_F(LabExamFixture, CheckNextType) {
+
+}
