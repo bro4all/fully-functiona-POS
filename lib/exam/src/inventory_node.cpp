439f4c88a65feaded6ba040ee6fc8852e8942772
Bro4all <omar.habra@sjsu.edu>
2018-03-22 17:53:28 -0700

    coding is fun: with Alex and Bryan.

--- lib/exam/src/inventory_node.cpp
+++ lib/exam/src/inventory_node.cpp
@@ -1,12 +1,15 @@
 #include "../inc/inventory_node.h"
 
 inventory_node::inventory_node(int input_upc, std::string input_name, int input_count, int input_price, int date) {
-    int upc=input_upc;
-    std::string name=input_name;
-    int inventory_count=input_count;
-    stack price;
-
+    upc = input_upc;
+    name = input_name;
+    inventory_count = input_count;
+    value_date a;
+    a.value = input_price;
+    a.date = date;
+    price.push(a);
     next = nullptr;
+    
 }
 
 inventory_node::~inventory_node()=default;
