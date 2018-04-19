088a0d2a0751f6338c30c7beca3c6fee6bd8bf8c
Bro4all <omar.habra@sjsu.edu>
2018-04-19 13:14:16 -0700

    10 EXTRA POINTS "CELEBRATION"

--- lib/exam/src/inventory.cpp
+++ lib/exam/src/inventory.cpp
@@ -1,6 +1,5 @@
-#include <cstdlib>
 #include "../inc/inventory.h"
-
+#include <cstdlib>
 int inventory::reserve_upc() {
     srand(42);
     auto random_upc_location = int(rand()%upc_generator.size());
@@ -10,7 +9,6 @@ int inventory::reserve_upc() {
     a->second = false;
     return a->first;
 }
-
 void inventory::release_upc(int input_upc) {
     auto val =  upc_generator.find(input_upc);
     if(val->first == input_upc && !val->second){
@@ -19,13 +17,11 @@ void inventory::release_upc(int input_upc) {
     }
     else throw "UPC not valid";
 }
-
 bool inventory::valid_upc(int input_upc) {
     if(input_upc == 0) return false;
     auto val = inventory::upc_generator.find(input_upc);
     return val->first == input_upc;
 }
-
 void inventory::initialize_upc() {
     srand(17); // Initial seed
     for(int i = 1; i< 1000; i++) {
@@ -33,156 +29,391 @@ void inventory::initialize_upc() {
         inventory::upc_generator.insert(std::pair<int, bool>(unique_upc, true));
     }
 }
-
-inventory::inventory() {
-head= nullptr;
+inventory::inventory()
+{
+    head  = nullptr;
+    initialize_upc();
 }
-
 inventory::~inventory() {
-inventory_node *tmp=head;
-    while(tmp!= nullptr){
-        tmp=tmp->next;
-        delete tmp;
-
+    while (head)
+    {
+        if(head->next == NULL)
+        {
+            head = nullptr;
+        }
+        else
+        {
+            inventory_node *temp = head->next;
+            delete head;
+            head = temp;
+        }
     }
 }
-
-void inventory::add_sku(std::string new_name, int initial_price, int initial_inventory, int initial_date) {
-if (head== nullptr){
-    head=new inventory_node(1,new_name,initial_inventory,initial_price,initial_date);
-}
-    else{
-    inventory_node*current=head;
-    while(current){
-        inventory_node* tmp=new inventory_node(reserve_upc(),new_name,initial_inventory,initial_price,initial_date);
-        current->next=tmp;
-        current=tmp;
+void inventory::add_sku(std::string new_name, int initial_price, int initial_inventory, int initial_date)
+{
+    int newupc = reserve_upc();
+    while (valid_upc(newupc) == false)
+    {
+        newupc = reserve_upc();
+    }
+    inventory_node *temp = new inventory_node (newupc, new_name, initial_inventory, initial_price, initial_date);
+    if (head == nullptr)
+    {
+        head = temp;
+    }
+    else
+    {
+        inventory_node *current = head;
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+        current->next = temp;
+        current = current->next;
     }
 }
-}
-
-void inventory::remove_sku(int input_upc) {
-inventory_node*previous;
-    inventory_node *current;
-    int count=0;
-    previous= nullptr;
-    current=head;
-    if(head== nullptr){
-        throw"this is empty yo";
-
+void inventory::remove_sku(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head ->next == nullptr)
+    {
+        head = nullptr;
     }
-    while(count !=input_upc){
-        previous=current;
-        current=current->next;
+    else if (head->upc == input_upc)
+    {
+        inventory_node *future = head->next;
+        delete head;
+        head = future;
     }
-    if(previous!= nullptr){
-        previous->next=current->next;
+    else
+    {
+        inventory_node *previous;
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            previous = current;
+            current = current->next;
+        }
+        previous ->next = current ->next;
         delete current;
     }
-    if(previous== nullptr){
-        head=current->next;
+}
+std::vector<int> inventory::get_upc(std::string input_name)
+{
+    std::vector<int> result;
+    inventory_node *current = head;
+    while (current != nullptr)
+    {
+        if (current->name == input_name)
+        {
+            result.push_back(current->upc);
+        }
+        current = current->next;
+    }
+    return result;
+}
+int inventory::get_price(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else{
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        return current->price.top().value;
     }
 }
-
-std::vector<int> inventory::get_upc(std::string input_name) {
-    inventory_node* curr=head;
-    while(curr!=NULL){
-        std::string strng=curr->name;
-        if(strng.compare(input_name)==0){
-            return  std::vector<int>();
+int inventory::get_inventory(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else{
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        return current->inventory_count;
+    }
+}
+std::string inventory::get_name(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else{
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        return current->name;
+    }
+}
+int inventory::get_lowest_price(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else
+    {
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        stack find;
+        find.operator=(current->price);
+        int low = find.top().value;
+        find.pop();
+        while (find.empty() == false)
+        {
+            if (find.top().value < low)
+                low = find.top().value;
+            find.pop();
+        }
+        return low;
+    }
+}
+int inventory::get_highest_price(int input_upc)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else
+    {
+        inventory_node *current = head;
+        while (current->upc != input_upc) {
+            current = current->next;
+        }
+        stack find;
+        find.operator=(current->price);
+        int high = find.top().value;
+        find.pop();
+        while (find.empty() == false) {
+            if (find.top().value > high)
+                high = find.top().value;
+            find.pop();
+        }
+        return high;
+    }
+}
+void inventory::adjust_price(int input_upc, int new_price, int new_date)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else{
+        struct value_date input;
+        input.date = new_date;
+        input.value = new_price;
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        current->price.push(input);
+    }
+}
+void inventory::adjust_inventory(int input_upc, int new_inventory)
+{
+    if (valid_upc(input_upc) == false)
+        throw -1;
+    else if (head == nullptr)
+    {
+        throw -1;
+    }
+    else{
+        inventory_node *current = head;
+        while (current->upc != input_upc)
+        {
+            current = current->next;
+        }
+        current->inventory_count = new_inventory;
+    }
+}
+void inventory::sort_by_lowest_price()
+{
+    inventory_node *current = head, *pre = nullptr, *next = nullptr;
+    inventory_node * top = nullptr, *pre1 = nullptr, *next1 = nullptr;
+    inventory_node *mini = nullptr, *mp = nullptr, *mn = nullptr;
+    while(current){
+        top = current->next;
+        mini = current;
+        while(top != nullptr){
+            if(mini->price.top().value > top->price.top().value){
+                mp = pre1;
+                mini = top;
+                mn = next1;
+            }
+            pre1 = top;
+            top = top->next;
+            if(top != nullptr) {
+                next1 = top->next;
+            }
+            else {
+                next1 = nullptr;
+            }
+        }
+        if(current->next == mini){
+            if(current == head){
+                current->next = mini->next;
+                mini->next = current;
+                current = mini;
+                head = mini;
+            }
+            else if (!mini->next){
+                current->next = nullptr;
+                mini->next = current;
+                pre->next = mini;
+                current = mini;
+            }
+            else{
+                current->next = mn;
+                mini->next = current;
+                pre->next = mini;
+                current = mini;
+            }
+        }
+        else if(current == head && current->next != mini){
+            if(!mini->next){
+                mini->next = current->next;
+                mp->next = current;
+                current->next = nullptr;
+                current = mini;
+                head = mini;
+            }
+            else if(mini->next && mp){
+                mini->next = current->next;
+                mp->next = current;
+                current->next = mn;
+                current = mini;
+                head = mini;
+            }
+            else if(!mp){
+                mini->next = current->next;
+                current->next = current;
+                current->next = mn;
+                current = mini;
+                head = mini;
+            }
+        }
+        else if(!mn && current->next != mini && current !=head){
+            current ->next = nullptr;
+            mp->next = current;
+            pre->next = mini;
+            mini->next = next;
+            current = mini;
+        }
+        else {
+            current ->next = mn;
+            mp->next = current;
+            pre->next = mini;
+            mini->next = next;
+            current = mini;
+        }
+        pre = current;
+        current = current->next;
+        if(current != nullptr) {
+            next = current->next;
+        }
+        else {
+            next = nullptr;
         }
     }
-    return std::vector<int>();
-}
-
-int inventory::get_price(int input_upc) {
-    inventory_node* curr=head;
-    while(curr!= nullptr){
-        if(curr->upc==input_upc){
-            return curr->price.top().value;
-        }
-        curr=curr->next;
+    inventory_node *a = head;
+    int length=0;
+    while(a){
+        int b = a->price.top().value;
+        int c = a->inventory_count;
+        a = a->next;
+        length ++;
     }
-    return 0;
-}
-
-int inventory::get_inventory(int input_upc) {
-    inventory_node* curr=head;
-    while(curr!= nullptr){
-        if(curr->upc==input_upc){
-            return curr->inventory_count;
+    inventory_node *x = head, *xp, *xn;
+    inventory_node *y = nullptr, *yp, *yn;
+    inventory_node *ls = nullptr, *lsp, *lsn;
+    while(x){
+        y = x->next;
+        ls = x;
+        while(y){
+            if(ls->price.top().value == y->price.top().value){
+                if(ls->inventory_count < y->inventory_count){
+                    lsp = yp;
+                    ls = y;
+                    lsn = yn;
+                }
+            }
+            yp = y;
+            y = y->next;
+            if(y != nullptr) {
+                yn = y->next;
+            }
+            else {
+                yn = nullptr;
+            }
         }
-        curr=curr->next;
-    }
-    return 0;
-}
-
-std::string inventory::get_name(int input_upc) {
-    inventory_node* curr=head;
-    while(curr->next!= nullptr){
-        curr=curr->next;
-        if(curr->upc==input_upc){
-            return  curr->name;
+        if(ls != x) {
+            if (x->next == ls) {
+                if (!lsn) {
+                    x->next = nullptr;
+                    xp->next = ls;
+                    ls->next = x;
+                    x = ls;
+                } else {
+                    x->next = lsn;
+                    ls->next = x;
+                    xp->next = ls;
+                    x = ls;
+                }
+            } else if (!lsn && x->next != ls) {
+                x->next = nullptr;
+                lsp->next =x;
+                xp->next =ls;
+                ls->next = xn;
+                x = ls;
+            } else {
+                x ->next = lsn;
+                lsp->next = x;
+                xp->next = ls;
+                ls->next = xn;
+                x = ls;
+            }
         }
-
-    }
-    return std::string();
-}
-
-int inventory::get_lowest_price(int input_upc) {
-    int count = 0;
-    inventory_node *curr;
-    curr = head;
-    int a = curr->price.top().value;
-    while(count <= input_upc){
-        if(curr->price.top().value < a) {
-            a = curr->price.top().value;
+        xp = x;
+        x = x->next;
+        if(x != nullptr){
+            xn = x->next;
         }
-        count ++;
-        curr=curr->next;
-    }
-    return a;
-}
-
-int inventory::get_highest_price(int input_upc) {
-    int count = 0;
-    inventory_node *curr;
-    curr = head;
-    int a = curr->price.top().value;
-    while(count >= input_upc){
-        if(curr->price.top().value < a) {
-            a = curr->price.top().value;
-        }
-        count ++;
-        curr=curr->next;
-    }
-    return a;
-}
-
-void inventory::adjust_price(int input_upc, int new_price, int new_date) {
-inventory_node* curr=head;
-    while(curr!= nullptr){
-        if(curr->upc==input_upc){
-            curr->price.pop();
-
+        else {
+            xn = nullptr;
         }
     }
-}
-
-void inventory::adjust_inventory(int input_upc, int new_inventory) {
-    inventory_node* curr=head;
-    while(curr->next!= nullptr){
-        curr=curr->next;
-        if(curr->upc==input_upc){
-            curr->inventory_count=new_inventory;
-        }
-    }
-}
-
-void inventory::sort_by_lowest_price() {
 
 }
-
-inventory_node *inventory::get_head() {
-    return nullptr;
+inventory_node *inventory::get_head()
+{
+    return head;
 }
